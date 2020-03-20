9872c14898430470da1e55c62f5e047369d3436c
Alex Post <alexpost1995@gmail.com>
2020-03-20 17:34:12 +0100

    Added sql script

---
+++ SQL script.sql
@@ -0,0 +1,19 @@
+insert into playlist(name, tracks) values("test playlist", "tracks");
+insert into playlist values(2, "test playlist 2", false, "nog meer tracks");
+insert into playlist values(3, "Nog een playlist", true, "Tracks tracks tracks");
+insert into playlist values(4, "Nog een playlist", true, "Tracks tracks tracks");
+insert into playlist values(5, "Nog een playlist", true, "Tracks tracks tracks");
+insert into playlist values(6, "Nog een playlist", true, "Tracks tracks tracks");
+
+insert into track(title, performer, duration, album) values ("Still Into You", "Paramore", 339, "Paramore");
+insert into track(title, performer, duration, album) values ("Playing God", "Paramore", 182, "Brand New Eyes");
+insert into track(title, performer, duration, album) values ("Brick By Boring Brick", "Paramore", 253, "Brand New Eyes");
+insert into track(title, performer, duration, album) values ("Welcome to the Black Parade", "My Chemical Romance", 253, "The Black Parade");
+delete from playlist where id = 7;
+
+delete from playlist where id > 0;
+select * from playlist;
+select * from track;
+use spotitube;
+
+ALTER TABLE track ADD FOREIGN KEY (playlistId) REFERENCES playlist(id);
