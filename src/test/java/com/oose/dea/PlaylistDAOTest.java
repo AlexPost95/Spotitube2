42fc61579901feb626b14f1bdadf287342b13a5f
Alex Post <alexpost1995@gmail.com>
2020-03-23 18:01:29 +0100

    Added DAO test getPlaylistByIdTest

--- src/test/java/com/oose/dea/PlaylistDAOTest.java
+++ src/test/java/com/oose/dea/PlaylistDAOTest.java
@@ -1,7 +1,9 @@
 package com.oose.dea;
 
+import com.oose.dea.api.Spotitube;
 import com.oose.dea.dao.PlaylistDAO;
 import com.oose.dea.domain.Playlist;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import javax.sql.DataSource;
@@ -9,27 +11,29 @@ import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.Mockito.*;
 
 public class PlaylistDAOTest {
 
+        PlaylistDAO playlistDAO = new PlaylistDAO();
+        DataSource dataSource = mock(DataSource.class);
+        Connection connection = mock(Connection.class);
+        PreparedStatement preparedStatement = mock(PreparedStatement.class);
+        ResultSet resultSet = mock(ResultSet.class);
+
+
     @Test
     public void getPlaylistByIdTest(){
         try {
             String expectedSQL = "select * from playlist where id = ?";
-            PlaylistDAO playlistDAO = new PlaylistDAO();
-            DataSource dataSource = mock(DataSource.class);
-            Connection connection = mock(Connection.class);
-            PreparedStatement preparedStatement = mock(PreparedStatement.class);
-            ResultSet resultSet = mock(ResultSet.class);
 
             when(dataSource.getConnection()).thenReturn(connection);
             when(connection.prepareStatement(expectedSQL)).thenReturn(preparedStatement);
             when(preparedStatement.executeQuery()).thenReturn(resultSet);
             when(resultSet.next()).thenReturn(false);
-
 
             playlistDAO.setDataSource(dataSource);
             int playlistId = 1;
@@ -38,6 +42,28 @@ public class PlaylistDAOTest {
             verify(dataSource).getConnection();
             verify(connection).prepareStatement(expectedSQL);
             verify(preparedStatement).setInt(1, playlistId);
+            verify(preparedStatement).executeQuery();
+
+        } catch (Exception e){
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void getPlaylistsTest(){
+        try {
+            String expectedSQL = "select * from playlist";
+
+            when(dataSource.getConnection()).thenReturn(connection);
+            when(connection.prepareStatement(expectedSQL)).thenReturn(preparedStatement);
+            when(preparedStatement.executeQuery()).thenReturn(resultSet);
+            when(resultSet.next()).thenReturn(false);
+
+            playlistDAO.setDataSource(dataSource);
+            ArrayList<Playlist> playlists = playlistDAO.getPlaylists();
+
+            verify(dataSource).getConnection();
+            verify(connection).prepareStatement(expectedSQL);
             verify(preparedStatement).executeQuery();
 
         } catch (Exception e){
