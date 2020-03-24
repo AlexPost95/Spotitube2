d38e7914768caa61236f5b6e787bcb3f5dcf21c1
Alex Post <alexpost1995@gmail.com>
2020-03-24 11:46:51 +0100

    Added DAO test getTracksTest

--- src/test/java/com/oose/dea/TrackDAOTest.java
+++ src/test/java/com/oose/dea/TrackDAOTest.java
@@ -10,6 +10,7 @@ import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.Mockito.*;
@@ -40,6 +41,30 @@ public class TrackDAOTest {
             verify(dataSource).getConnection();
             verify(connection).prepareStatement(expectedSQL);
             verify(preparedStatement).setInt(1, trackId);
+            verify(preparedStatement).executeQuery();
+
+        } catch (Exception e){
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void getTracksTest(){
+        try {
+            String expectedSQL = "select * from track where id not in (select t.id from track t inner join playlisttracks pt on t.id = pt.trackId where pt.playlistId = ?)";
+
+            when(dataSource.getConnection()).thenReturn(connection);
+            when(connection.prepareStatement(expectedSQL)).thenReturn(preparedStatement);
+            when(preparedStatement.executeQuery()).thenReturn(resultSet);
+            when(resultSet.next()).thenReturn(false);
+
+            trackDAO.setDataSource(dataSource);
+            int playlistId = 1;
+            ArrayList<Track> tracks = trackDAO.getTracks(playlistId);
+
+            verify(dataSource).getConnection();
+            verify(connection).prepareStatement(expectedSQL);
+            verify(preparedStatement).setInt(1, playlistId);
             verify(preparedStatement).executeQuery();
 
         } catch (Exception e){
